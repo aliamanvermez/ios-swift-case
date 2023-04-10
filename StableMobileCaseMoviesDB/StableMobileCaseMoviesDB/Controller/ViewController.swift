98c3dbee8787e194efbc20c05f7f94c6f398f2e5
Ali Amanvermez <maliamanvermez@gmail.com>
2023-04-10 17:52:00 +0300

    Initial commit. Programmatic UI infrastructure prepared.

---
+++ StableMobileCaseMoviesDB/StableMobileCaseMoviesDB/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StableMobileCaseMoviesDB
+//
+//  Created by Ali Amanvermez on 10.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
