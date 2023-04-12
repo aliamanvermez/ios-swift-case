155e58f8e053f0e2c92417225ec2c41493237f81
Ali Amanvermez <maliamanvermez@gmail.com>
2023-04-12 22:09:02 +0300

    Show Detail raw contsructure implemented

---
+++ StableMobileCaseMoviesDB/StableMobileCaseMoviesDB/Controller/MDBShowDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MDBShowDetailViewController.swift
+//  StableMobileCaseMoviesDB
+//
+//  Created by Ali Amanvermez on 12.04.2023.
+//
+
+import UIKit
+
+class MDBShowDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ StableMobileCaseMoviesDB/StableMobileCaseMoviesDB/View Model/MDBShowDetailViewViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MDBShowDetailViewViewModel.swift
+//  StableMobileCaseMoviesDB
+//
+//  Created by Ali Amanvermez on 12.04.2023.
+//
+
+import Foundation
+
+final class MDBTvShowListDetailViewViewModel{
+    
+}
